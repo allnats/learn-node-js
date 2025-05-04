9497a4c456892f14f8a242c9236027e6546afa1f
Allendale Nato <anato2@academic.rrc.ca>
2025-05-04 12:15:54 -0500

    feat: Add npm packages validator and chalk in Notes App project.

--- Projects/01 Notes App/app.js
+++ Projects/01 Notes App/app.js
@@ -1,5 +1,39 @@
-const fs = require("fs")
+// Relative path always start with a 'dot slash'.
+const my_utils = require("./utils")
+const getNotes = require("./notes")
+const validator = require("validator")
+const chalk = require("chalk")
+
+/* ES Module alternative
+
+import "./utils"
+
+*/
+
+const my_name = "Allendale"
+
+console.log(my_utils.name)
+console.log(my_utils.add(2, 3))
+console.log(getNotes())
+
+let some_email = "allendale@nato.com"
+let some_address = "example.com"
+
+const msg_ok = chalk.green.inverse
+const msg_fail = chalk.red.inverse
 
 
-fs.writeFileSync("notes.txt", "This file was created by Node.js\n")
-fs.appendFileSync("notes.txt", "My name is Allen!")
+console.log(
+    `Is ${some_email} a valid email address?`,
+    msg_ok(validator.isEmail(some_email))
+)
+
+console.log(
+    `Is ${some_address} a valid email address?`,
+    msg_fail(validator.isEmail(some_address))
+)
+
+console.log(
+    `Is ${some_address} a valid URL?`,
+    msg_ok(validator.isURL(some_address))
+)
