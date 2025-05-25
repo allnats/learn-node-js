035023fedfa9788d6bf7aa9fed915178b3d9c027
Allendale Nato <anato2@academic.rrc.ca>
2025-05-24 20:54:10 -0500

    fix: Rename Notes App directory to notes-app.

---
+++ Projects/notes-app/app.js
@@ -0,0 +1,76 @@
+// Relative path always start with a 'dot slash'.
+const yargs = require("yargs")
+const validator = require("validator")
+const chalk = require("chalk")
+const my_utils = require("./utils")
+const notes = require("./notes").default
+
+
+// Set the version of your program.
+// You can display this with the --version flag.
+yargs.version("1.1.0")
+
+// Create add command
+yargs.command({
+    command: "add",
+    describe: "Adds a new note!",
+    builder: {
+        title: {
+            describe: "Note's title",
+            demandOption: true, // Set to required
+            type: "string"
+        },
+        body: {
+            describe: "Note's contents",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// Create remove comand.
+yargs.command({
+    command: "remove",
+    describe: "Removes a note!",
+    builder: {
+        title: {
+            describe: "Note's title",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// Create list comand.
+yargs.command({
+    command: "list",
+    describe: "List your notes!",
+    handler() {
+        notes.getNotes()
+    }
+})
+
+// Create read comand.
+yargs.command({
+    command: "read",
+    describe: "Read a note!",
+    builder: {
+        title: {
+            describe: "Note's title",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
+// console.log(yargs.argv)
---
+++ Projects/notes-app/utils.js
@@ -0,0 +1,10 @@
+// console.log("utils.js");
+
+const name = "Jane"
+
+const add = (a, b) => a + b;
+
+module.exports = {
+    name,
+    add
+}
