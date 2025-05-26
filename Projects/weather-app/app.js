8574463630fed3cca9bbf33fa79436fdfead7195
Allendale Nato <anato2@academic.rrc.ca>
2025-05-26 14:47:02 -0500

    feat: Implement simple HTTP API request.

--- Projects/weather-app/app.js
+++ Projects/weather-app/app.js
@@ -1,11 +1,17 @@
-console.log("Starting")
+import request from "postman-request"
+import utils from "./utils.js"
 
-setTimeout(() => {
-    console.log("2 second timer")
-}, 2000)
+const apiKey = utils.getAPIKey()
 
-setTimeout(() => {
-    console.log("0 second timer")
-}, 0)
+const weatherStackURL = "https://api.weatherstack.com/current"
+const query = "Winnipeg"
+const apiReq = `${weatherStackURL}?access_key=${apiKey}&query=${query}`
 
-console.log("Stopping")
+request(apiReq, (err, res) => {
+    if (err) {
+        console.log(err.message)
+    } else {
+        const data = JSON.parse(res.body)
+        console.log(data)
+    }
+})
